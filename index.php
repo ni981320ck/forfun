0c164dc30715c799633123ab2ac62b80b6e34f90
ni981320ck <ni981320ck@gmail.com>
2016-09-24 19:08:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,25 +24,38 @@
 		    </div>
 
 			<div id="leftfacebook">
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FYui.Hirasawa.320%2Fposts%2F1229749303714524&width=500" width="500" height="696" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>
+				<div id="four">
+					<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FYui.Hirasawa.320%2Fposts%2F1229749303714524&width=500" width="500" height="696" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>
+				</div>
+		        
+				<div id="two">
+					<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fschairnatsuki%2Fposts%2F972507429535178&width=500" width="500" height="590" style="border:none;overflow:hidden" scrolling="yes" frameborder="0" allowTransparency="true"></iframe>
+				</div>
 
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fschairnatsuki%2Fposts%2F972507429535178&width=500" width="500" height="590" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		    
-		    	<blockquote class="twitter-tweet" data-lang="zh-tw"><p lang="zh" dir="ltr">【晚安】去年的今天，謝謝妳。</p>&mdash; Yue Nai (@Yue_Nai) <a href="https://twitter.com/Yue_Nai/status/778038540370268160">2016年9月20日</a></blockquote>
-			<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				<div id="twitter">
+					<blockquote class="twitter-tweet" data-lang="zh-tw"><p lang="ja" dir="ltr">天冷了 想喝杯咖啡 等一個人...<a href="https://t.co/D6fcEIZJqQ">pic.twitter.com/D6fcEIZJqQ</a><br><br>Hiten◆C1あ-75b <a href="https://twitter.com/HitenKei">@HitenKei</a></p>&mdash; Yue Nai (@Yue_Nai) <a href="https://twitter.com/Yue_Nai/status/710370120405688322">2016年3月17日</a></blockquote>
+					<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				</div>
+
 		    </div>
 
 			<div id="rightfacebook">
-		    	紀錄遊戲專用帳號 ヽ(✿ﾟ▽ﾟ)ノ<br>
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1793812907543255&width=500" width="250" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1794727770785102%3A0&width=500" width="250" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>	
+				<div id="three">
+					紀錄遊戲專用帳號 ヽ(✿ﾟ▽ﾟ)ノ<br>
+			        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1793812907543255&width=500" width="250" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+			        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1794727770785102%3A0&width=500" width="250" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>	
+				</div>
 
-		        [2015/10] League of Legends 精華第三集<br>
-		        <iframe width="500" height="281" src="https://www.youtube.com/embed/XwmZaVx6B9k" frameborder="0" allowfullscreen></iframe><br>
+				<div id="one">
+					[2015/10] League of Legends 精華第三集<br>
+		        	<iframe width="500" height="281" src="https://www.youtube.com/embed/XwmZaVx6B9k" frameborder="0" allowfullscreen></iframe><br>
+				</div>
 
-		        <br><br>網頁設計的小夥伴們<br>
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D1244126928955168%26set%3Da.148105701890635.26679.100000736195394%26type%3D3&width=500" width="250" height="609" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D627772247403890%26set%3Da.487088774805572.1073741826.100005133601569%26type%3D3&width=500" width="250" height="608" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+				<div id="zero">
+					<br><br>網頁設計的小夥伴們<br>
+		        	<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D1244126928955168%26set%3Da.148105701890635.26679.100000736195394%26type%3D3&width=500" width="250" height="609" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+		        	<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D627772247403890%26set%3Da.487088774805572.1073741826.100005133601569%26type%3D3&width=500" width="250" height="608" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+				</div>
 		    </div>
 	</div>
 
