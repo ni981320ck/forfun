68d9d2d3ad609cb07a19328975f7cf4d55fbc821
ni981320ck <ni981320ck@gmail.com>
2016-11-03 00:24:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,77 +3,41 @@
 <head>
     <title>Love Natsuki</title>
     <link rel="shortcut icon" href="icon.ico">
-    <link rel="stylesheet" type="text/css" href="./index.css">
-    <link rel="stylesheet" type="text/css" href="./bat.css">
-    <link rel="stylesheet" type="text/css" href="./EasterEgg.css">
+    <link rel="stylesheet" type="text/css" href="./body.css">
 </head>
 <body>
 <div id="background"></div>
-<div class="bat"></div>
 
-<div id="river">
-	<h1>Pinkie Nai</h1>
-			<div id="trunk">
-			<div id="branches4"></div>
-			<div id="branches3"></div>
-			<div id="branches25"></div>
-			<div id="branches2"></div>
-			<div id="branches1"></div>
-			<div id="branches0"></div>
-			<div id="branchestwitter"></div>
-		</div>
+<div id="sidebar"></div>
 
-		<div id="follow">
-		    <iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2FYui.Hirasawa.320&width=100&height=65&layout=box_count&size=large&show_faces=true&appId=562564170420300" width="100" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		</div>
+<div id="content"></div>
 
-		<div id="box">
-		    <div id="form1">
-		    	<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FYui.Hirasawa.320%2Fposts%2F1242599679096153&width=500" width="500" height="642" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		    </div>
-		    <div id="form2">
-		    	<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D958116410974280%26set%3Da.436842666434993.1073741827.100003277907696%26type%3D3&width=500" width="500" height="661" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		    </div>
-		</div>
-
-		<div id="leftfacebook">
-			<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FYui.Hirasawa.320%2Fposts%2F1229749303714524&width=500" width="500" height="696" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>
-				
-			<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fschairnatsuki%2Fposts%2F972507429535178&width=500" width="500" height="590" style="border:none;overflow:hidden" scrolling="yes" frameborder="0" allowTransparency="true"></iframe>
-
-			<section id="btn">
-				<p>
-					<div>
-						<button onclick="OpenEgg()"> (　ﾟ∀ﾟ)つ ♥</button>
-					</div>
-				</p>
-				<div id="facebook">
-					<div class="logo"><div class="recto"></div></div>
-					<div class="top"></div>
-					<div class="logo verso"></div>
-				</div>
-				<div id="shadow"><img src="shadow.png" alt="shadow" /></div>
-			</section>
-
-			<blockquote class="twitter-tweet" data-lang="zh-tw"><p lang="ja" dir="ltr">天冷了 想喝杯咖啡 等一個人...<a href="https://t.co/D6fcEIZJqQ">pic.twitter.com/D6fcEIZJqQ</a><br><br>Hiten◆C1あ-75b <a href="https://twitter.com/HitenKei">@HitenKei</a></p>&mdash; Yue Nai (@Yue_Nai) <a href="https://twitter.com/Yue_Nai/status/710370120405688322">2016年3月17日</a></blockquote>
-			<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-	    </div>
-
-		<div id="rightfacebook">
-			紀錄遊戲專用帳號 ヽ(✿ﾟ▽ﾟ)ノ<br>
-		    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1793812907543255&width=500" width="247.5" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FPinkieNai%2Fposts%2F1794727770785102%3A0&width=500" width="247.5" height="588" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-		    找彩蛋囉 (　ﾟ∀ﾟ)つ
-		    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-				
-			【史上最棒造型】Star Guardian Jinx 星光少女 吉茵珂絲 <br>
-	        <iframe width="500" height="281" src="https://www.youtube.com/embed/T0yeBCOS6dk" frameborder="0" allowfullscreen></iframe>
-				
-			<br><br>網頁設計的小夥伴們<br>
-	        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D1244126928955168%26set%3Da.148105701890635.26679.100000736195394%26type%3D3&width=500" width="247.5" height="609" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-	        <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D627772247403890%26set%3Da.487088774805572.1073741826.100005133601569%26type%3D3&width=500" width="247.5" height="608" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-	    </div>
+<div id="footer">
+	<div id="copyright">Copyright © 2016 PinkieNai. All rights reserved.</div>
 </div>
-	<script type="text/javascript" src="index.js"></script>
+
+<div id="header">
+	<div id="menu">
+	<ul>
+		<li><a href="about.php"><br>ABOUT</a></li>
+	 	<li class="menudeco"></li>
+		<li><a href="video.php"><br>VIDEO</a></li>
+		<li class="menudeco"></li>
+		<li><a href="gamePM.php"><br>GAME</a></li>
+		<li class="menudeco"></li>
+		<li><a href="http://pinkienai.website/"><br>　　　　　　　　　　　　　　</a></li>
+		<li class="menudeco"></li>
+		<li><a href="link.php"><br>LINK</a></li>
+		<li class="menudeco"></li>
+		<li><a href="store.php"><br>STORE</a></li>
+		<li class="menudeco"></li>
+		<li><a href="pony.php"><br>PONY</a></li>
+		<li class="menudeco"></li>
+	 </ul>
+	</div>
+</div>
+
+<div id="title"><img src="title.png" width="500"/></div>
+
 </body>
 </html>
